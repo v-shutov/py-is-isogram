72640a3cd3f6cdb5de48e6669ccfce0c669dac8d
Volodymyr Shutov <forshutov@gmail.com>
2024-05-06 18:14:40 +0400

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        pytest.param(
+            "", True,
+            id="should be True when empty sting"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="should be case-insensitive"
+        ),
+        pytest.param(
+            "playgrounds", True,
+            id="should be True if no repeating letters"
+        ),
+        pytest.param(
+            "adam", False,
+            id="should be False if is repeating letters non-consecutive"
+        ),
+        pytest.param(
+            "three", False,
+            id="should be False if is repeating letters consecutive"
+        )
+    ]
+)
+def test_function(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
